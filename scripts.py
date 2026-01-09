4ec0605b310c1afd3d9c659ec3225907af2e1bf4
EL Mustapha <elmustaphaabimbola005@gmail.com>
2026-01-09 16:06:03 +0100

    restructured the code

--- scripts.py
+++ scripts.py
@@ -1,31 +1,55 @@
+""" Functions for managing the system 
+grade_system(user_score : float) - This function returns the user's grade point (0.0 to 5.0)
+grade_scale(user_score: str) - returns the student's grade scale ('A'-'F')
+course_point(units, user_score : float) - returns the total score = unit * user_score
+"""
+
 def grade_system(user_score):
 
-    """This function checks the grade and points of a user in a university system"""
-
-    grade_score = {
-            "A" : 5.0,
-            "B" : 4.0,
-            "C" : 3.0,
-            "D" : 2.0,
-            "E" : 1.0,
-            "F" : 0.0,
-     }
+    if user_score < 0 and user_score > 100 : 
+        raise ValueError ("Invalid input. Scores must be between 0-100")
     
-    if user_score >= 70.0 and user_score < 100 :
-        print(grade_score["A"])
-    elif user_score >= 60.0 and user_score < 69.999 : 
-        print(grade_score["B"])    
-    elif user_score >= 50.0 and user_score < 59.999 : 
-        print(grade_score["C"])    
-    elif user_score >= 45.0 and user_score < 49.999: 
-        print(grade_score["D"])    
-    elif user_score >= 40.0 and user_score < 44.999 : 
-        print(grade_score["E"])    
+    if not isinstance(user_score ,(int , float)) : 
+        raise TypeError ("Error!. Enter a number")
+    
 
+    if user_score >=  70.00 :
+        return 5.0 
+    if user_score >= 60.00 : 
+        return 4.0 
+    if user_score >= 50.00 : 
+        return 3.0 
+    if user_score >= 45.00 : 
+        return 2.0
+    if user_score >= 40.00 : 
+        return 1.0 
     else : 
-        print(grade_score["F"])   
+        return 0.0
+    
+def grade_point(user_score) : 
 
+    if user_score < 0 and user_score > 100 : 
+        raise ValueError ("Invalid input. Scores must be between 0-100")
+    
+    if not isinstance(user_score ,(int , float)) : 
+        raise TypeError ("Error!. Enter a number")
 
-def user_input(user_score) : 
-    input("Enter your grades to check your results! : ")
+    if user_score >=  70.00 :
+        return "A"
+    if user_score >= 60.00 : 
+        return "B" 
+    if user_score >= 50.00 : 
+        return "C" 
+    if user_score >= 45.00 : 
+        return "D"
+    if user_score >= 40.00 : 
+        return "E" 
+    else : 
+        return "F"
+    
+def total_score(units , user_score) : 
+    if units < 0 : 
+        raise ValueError ("Invalid") 
+    return units * grade_system(user_score)
+       
 
