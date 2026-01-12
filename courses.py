75554308923cf00c750f41c5d86b0654d13553b5
EL Mustapha <elmustaphaabimbola005@gmail.com>
2026-01-12 21:55:29 +0100

    Commit

--- courses.py
+++ courses.py
@@ -1,11 +1,17 @@
-from scripts import grade_system 
+from scripts import grade_system , grade_point , total_score 
 
 class Courses :
-    def __init__(self , course_code , course_units, grade_system) : 
+    def __init__(self , course_code  : str, course_units : float , grade_score : float) : 
         self.course_code = course_code 
-        self.course_units = course_units
-        self.grade_system = grade_system 
+        self.course_units = float(course_units)
+        self.grade_score = float(grade_score)
 
 
-#Getting inputs from users 
-num
+def grade_scale(self) : 
+    """return the total score for the weighted score point"""
+    return total_score(self.course_units , self.grade_score) 
+
+def grade(self) : 
+    """returns the equivaelent letter scale based on score"""
+    return grade_point(self.grade_score) 
+
